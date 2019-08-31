330a66871f21cf550885bccd49b5004a9e7e0d42
wuchch <wucc@dragonpass.com.cn>
2019-08-31 14:09:17 +0800

    [fix]测试

--- demo-system/demo-system/web/src/main/java/com/manage/system/controller/TypeController.java
+++ demo-system/demo-system/web/src/main/java/com/manage/system/controller/TypeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * @author wucc
  * @date 2019/8/30 17:37
