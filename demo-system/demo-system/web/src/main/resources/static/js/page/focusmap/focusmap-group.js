4fb8e41a043ad583676695210335cf6f1eb2cc67
wuchch <563576277@qq.com>
2019-09-04 10:09:23 +0800

    [feat]代码提交

---
+++ demo-system/demo-system/web/src/main/resources/static/js/page/focusmap/focusmap-group.js
@@ -0,0 +1,100 @@
+$(function () {
+    //1.初始化Table
+    var oTable = new TableInit();
+    oTable.Init();
+
+    //2.初始化Button的点击事件
+    var oButtonInit = new ButtonInit();
+    oButtonInit.Init();
+
+});
+
+var TableInit = function () {
+    var oTableInit = new Object();
+    //初始化Table
+    oTableInit.Init = function () {
+        $('#tb_body').bootstrapTable({
+            url: '/focusMap/findGroupList',                 //请求后台的URL（*）
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            method: 'post',                      //请求方式（*）
+            toolbar: '#toolbar',                //工具按钮用哪个容器
+            striped: true,                      //是否显示行间隔色
+            cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+            pagination: false,                   //是否显示分页（*）
+            sortable: true,                    //是否启用排序
+            sortOrder: "asc",                   //排序方式
+            queryParams: oTableInit.queryParams,//传递参数（*）
+            sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+            pageNumber: 1,                       //初始化加载第一页，默认第一页
+            pageSize: 5,                       //每页的记录行数（*）
+            pageList: [5, 10, 30],        //可供选择的每页的行数（*）
+            search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            strictSearch: true,
+            // showColumns: true,                  //是否显示所有的列
+            showRefresh: true,                  //是否显示刷新按钮
+            minimumCountColumns: 2,             //最少允许的列数
+            clickToSelect: true,                //是否启用点击选中行
+            height: 550,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+            showToggle: false,                    //是否显示详细视图和列表视图的切换按钮
+            cardView: false,                    //是否显示详细视图
+            detailView: false,                   //是否显示父子表
+            onLoadSuccess: function(result){
+                if(result != null && result.data != null) {
+                    $("#table").bootstrapTable("load",result.data);
+                }else {
+                    $("#table").bootstrapTable("load",[]);
+                }
+            },
+            columns: [
+                {
+                    field: '',
+                    title: '序号',
+                    formatter: function (value, row, index) {
+                        return index+1;
+                    }
+                },
+                {
+                    field: 'name',
+                    title: '焦点图位置'
+                },{
+                    field: 'num',
+                    title: '焦点图数量'
+                },{
+                    field: 'code',
+                    title: '操作',
+                    cellStyle: {'css': {'text-align': 'center', 'width': '220px'}},
+                    formatter: function (value, row, index) {
+                        var standard = '<button type="button" style="margin-right: 15px" class="btn btn-success" onclick="manageModal(\'' + value + '\')">管理</button>';
+                        // var order = '<button type="button" class="btn btn-danger" onclick="deleteFunction(\'' + value + '\')">删除</button>';
+                        return standard;
+                    }
+                }
+            ]
+        });
+    };
+    //得到查询的参数
+    oTableInit.queryParams = function (params) {
+
+        var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            'pageNumber': this.pageNumber,  //页面大小
+            'pageSize': this.pageSize       //页码
+        };
+        return temp;
+    };
+    return oTableInit;
+};
+
+var ButtonInit = function () {
+    var oInit = new Object();
+    var postdata = {};
+    oInit.Init = function () {
+        //初始化页面上面的按钮事件
+    };
+    return oInit;
+};
+
+function manageModal(code) {
+    console.log(code);
+    parent.window.addTab(2, '焦点图详情', 'focusmap/focusmapList.html?code=' + code);
+}
